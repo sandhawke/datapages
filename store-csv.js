64432ceb79d46ecae85acd4ba709028f973766d8
Sandro Hawke <sandro@hawke.org>
2017-09-30 21:25:11 -0400

    somewhat implemented clever lazy start

--- store-csv.js
+++ store-csv.js
@@ -1,8 +1,13 @@
 'use strict'
 
+// Just implements the PRIMITIVE interface: on-change, on-save,
+// create(no args), listenSince, setProperty
+//
+// If you want more, or caching, bridge it to InMem
+
 const EventEmitter = require('eventemitter3')
 const debugM = require('debug')
-var fs = require('fs');
+const fs = require('fs')
 const parse = require('csv-parse')
 const stringify = require('csv-stringify')
 
@@ -18,46 +23,107 @@ class StoreCSV extends EventEmitter {
     if (!this.debug) this.debug = debugM('datapages_store_csv_' + this.debugName)
 
     this.str = stringify()
+
+    this.nextID = undefined // these count up from max in file
+    this.nextTempID = -1    // these count DOWN, -1, -2, -3, ...
+
+    this.fileA = fs.openSync(this.filename, 'a')
+
+    this.firstListenStarted = false
+    this.firstListenEnded = false
+    this.maxSubjectIDSeen = 0
+    this.maxDeltaIDSeen = 0
   }
 
+  /*
+    Return a new handle for an object; that's all we really have to
+    do.  We use numbers.
+
+    To make this synchronous before boot() has resolved, we have to do
+    the temp-id trick.  Probably silly for this module, but it's
+    definitely a trick we want for network usage.
+  */
   create () {
-    return { __placeholder: true }
+    let id = this.nextID
+    if (id) {
+      this.nextID++
+    } else {
+      this.debug('create() before learning nextID, using temp')
+      id = --this.nextTempID
+    }
   }
 
+  /*
+    Read the file, streaming out the deltas
+  */
   listenSince (seq, name, cb) {
-    // open file and read it, subject = line-number
+    if (name !== 'change') throw Error('only "change" events implemented in listenSince')
 
-    var parser = parse({delimiter: ':'})
-    var input = fs.createReadStream(this.filename);
+    const first = !this.firstListenStarted
+    this.firstListenStarted = true
+
+    const parser = parse()
+    const input = fs.createReadStream(this.filename);
     const sink = new EventEmitter()
-    sink.write = (obj) => {
-      cb(obj.subject, obj)
+    sink.write = (delta) => {
+      this.maxSubjectIDSeen = Math.max(this.maxSubjectIDSeen, delta.subject)
+      this.maxDeltaIDSeen = Math.max(this.maxDeltaIDSeen, delta.seq)
+      cb(delta.subject, delta)
     }
     sink.end = () => {
+      if (first) {
+        this.nextCreate = this.maxSubjectSeenID + 1
+        this.nextDelta = this.maxDeltaSeenID + 1
+        this.firstListenEnded = true
+        this.emit('booted')
+      }
+
+      // in theory, things might be in the output pipeline and we'd
+      // miss them.  Have them in a being-written buffer?
+      
       this.emit('stable')  // replay-done?
       this.debug('done')
+      input.close()
     }
 
     input.pipe(parser).pipe(sink)
   }
 
-  setProperty (subject, property, value, who, when) {
-    if (typeof subject === 'object') {
-      if (subject.__assignedID) {
-        subject = subject.__assignedID
-      } else if (subject.__placeholder) {
-        await idDispender, set subject.id
-      } else {
-        throw Error('bad first argument to setProperty')
-      }
+  boot () {
+    return new Promise((resolve, reject) => {
+      if (this.firstListenEnded) resolve()
+      this.on('booted', resolve)
+      // let someone who's going to use the data go first
+      process.nextTick(() => {
+        if (this.firstListen) {
+          this.listenSince(0, 'change', () => null)
+        }
+      })
+    })
+  }
+  
+  // which is better? 
+  // async applyDelta(delta) {
+  //     let {subject, property, value, who, when} = delta
+
+  async setProperty (subject, property, value, who, when) {
+    if (subject < 0) {
+      await this.boot()
+      subject = await this.permID(subject)
     }
-    // if simple number, then fine, append this delta
+    const seq = ++this.seq
+
+    const delta = {subject, property, value, who, when}
     const record = [subject, property, value, who, when]
     stringify([record], (err, output) => {
       this.debug('csv: ', output)
-
-      // when it's written, emit  subject save    details about file
+      // // FOR NOW use filename, so it's closed immediately
+      fs.appendFile(this.fileA, output, {encoding: 'utf8'}, (err) => {
+        if (err) throw err
+        this.emit('save', delta, { filename: this.filename })
+      })
     })
+    this.emit('change', delta.subject, delta)
   }
 }
 
--- test-store.js
+++ test-store.js
@@ -1,10 +1,14 @@
 'use strict'
 
+// factor out test-primative ?
+
 const test = require('tape')
 const InMem = require('./inmem').InMem
 const Bridge = require('./bridge').Bridge
 const StoreCSV = require('./store-csv').StoreCSV
 const debug = require('debug')('datapages_test_store')
+const fs = require('fs')
+const path = require('path')
 
 test('read', t => {
   const db = new StoreCSV('/etc/passwd')
@@ -16,8 +20,50 @@ test('read', t => {
   })
 })
 
-test.only('setProperty', t => {
-  const db = new StoreCSV('/tmp/t1')
-  db.setProperty(1, 'age', 20)
+test.only('using temporary files', tt => {
+  fs.mkdtemp('/tmp/datapages-test-', (err, tmp) => {
+    if (err) throw err;
+    tt.comment('running file tests in: ' + tmp)
+    
+    tt.test('applyDelta', t => {
+      const file = path.join(tmp, 't1')
+      const db = new StoreCSV(file)
+      db.on('save', delta => {
+        t.equal(delta.value, 20)
+        const written = fs.readFileSync(file, 'utf8')
+        t.equal(written, '1,age,20,,\n')
+        t.end()
+      })
+      db.setProperty(1, 'age', 20)
+    })
+
+    tt.test('applyDelta 3', t => {
+      const file = path.join(tmp, 't2')
+      const db = new StoreCSV(file)
+      db.on('save', delta => {
+        if (delta.value === 40) {
+          const written = fs.readFileSync(file, 'utf8')
+          t.equal(written, '1,age,21,,\n2,age,22,,\n1,age,40,,\n')
+          t.end()
+        }
+      })
+      db.setProperty(1, 'age', 21)
+      db.setProperty(2, 'age', 22)
+      db.setProperty(1, 'age', 40)
+    })
+
+    tt.test('read deltas', t => {
+      const file = path.join(tmp, 'read-deltas')
+      const text = '1,age,21,,\n2,age,22,,\n1,age,40,,\n'
+      fs.writeFileSync(file, text, 'utf8')
+      const db = new StoreCSV(file)
+      db.listenSince(0, 'change', (pg, delta) => {
+        debug('heard', delta)
+      })
+      db.on('stable', () => {
+        t.end()
+      })
+    })
+  })
 })
 
