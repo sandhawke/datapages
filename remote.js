584e23ebb538642bc1284a5ba9eb6ff00b308215
Sandro Hawke <sandro@hawke.org>
2017-10-17 17:52:07 -0400

    add proxyFor; make applyDelta add sessionID if missing

--- remote.js
+++ remote.js
@@ -10,6 +10,17 @@ class Remote extends InMem {
     this.myBridge = this.bridge(this.rawClient)
   }
 
+  applyDelta (delta) {
+    // put our sessionID in asap, so local code can stop caring if
+    // this delta is from local or remote.
+    if (delta.who === undefined) delta.who = this.sessionID
+    return super.applyDelta(delta)
+  }
+  
+  get sessionID () {
+    return this.rawClient.transport.sessionData.id
+  }
+  
   // make this async, waiting for sessionActive
   // OR make it an empty proxy, with values that get filled it?
   // that's harder
@@ -21,6 +32,13 @@ class Remote extends InMem {
     r = this.myBridge.reach(this.rawClient, this, r.id)
     this.debug('returning sessionData', r)
     return r
+  }
+
+  proxyFor (id) {
+    // I *think* reach can & should MAKE IT if it doesn't exist yet
+    const res = this.myBridge.reach(this.rawClient, this, id)
+    this.debug('proxy for %d is %o', id, res)
+    return res
   }
 
   async waitForSessionProperty (prop, timeout) {
